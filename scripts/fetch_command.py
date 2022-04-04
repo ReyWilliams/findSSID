da2e22d007d21f6f1019533aa0047d8d5f2af4ba
ReyWilliams <70685535+ReyWilliams@users.noreply.github.com>
2022-04-03 22:09:53 -0400

    added script to fetch command and compare times

---
+++ scripts/fetch_command.py
@@ -0,0 +1,35 @@
+import time
+import requests
+ 
+while True:
+    epoch_time = int(time.time() * 1000) # get current time since epoch in milliseconds
+
+    # fetcht the command and date from endpoint
+    r = requests.get("https://data.mongodb-api.com/app/wifirobot-zpufi/endpoint/config/getComm")
+
+    # store response
+    res = r.json()
+
+    # store command
+    command = res[0]["command"]
+
+    # store date (time command submitted in milliseconds)
+    date = int(res[0]["date"]["$date"]["$numberLong"])
+
+
+    # calculate difference in seconds
+    seconds = (epoch_time - date)/1000
+
+    # print statements
+    # print(date)
+    # print(epoch_time)
+    # print(seconds)
+
+    # if command was submitted 30s or less, write command to file
+    if(seconds <= 30):
+        f = open("COMM.txt", "w")
+        f.write(command)
+        f.close()
+
+    # sleep for 30s
+    time.sleep(30)
