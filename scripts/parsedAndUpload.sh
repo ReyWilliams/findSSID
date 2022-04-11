db751a529fb97dd347e2e33d8524a5125ab998a1
Rey Williams <reynaldomwilliams@gmail.com>
2022-04-11 13:43:46 -0400

    updating scripts

--- scripts/parsedAndUpload.sh
+++ scripts/parsedAndUpload.sh
@@ -3,7 +3,9 @@
 while true
 do
 	cd /home/pi/Desktop/Parsing
-	python parsing.py
+	#python parsing.py
+	python fetch_command.py
+	python acknowledge.py
 	echo "Fetched Parsed and Uploaded"
 	sleep 30
 done
--- scripts/parsing.py
+++ scripts/parsing.py
@@ -10,32 +10,56 @@ def getBitRatesVals(bitRateString): #splits bit rate string into array of number
     return changedVals
 
 endpoint = 'https://data.mongodb-api.com/app/wifirobot-zpufi/endpoint/entries/uploadEntry'
+POSendpoint = 'https://data.mongodb-api.com/app/wifirobot-zpufi/endpoint/entries/uploadPOSEntry'
 
 networks = get_interfaces(interface='wlan0')
-for network in networks:
-    print(network['Name'])    
-    name = network['Name']
-    address = network['Address']
 
-    signalLevel = network['Signal Level']
-    signalLevel = int(signalLevel) #get numeric value
+try:
+    # open file to read last DATA recieved 
+    f = open("DATA.txt", "r")
+    gridAndPos = f.read().split(' ')
 
-    bitRates = network['Bit Rates']
+    gridX = gridAndPos[0]
+    gridY = gridAndPos[1]
+    posX = gridAndPos[2]
+    posY = gridAndPos[3]
 
-    #changes bit rate string to array of values
-    bitRates = getBitRatesVals(bitRates) 
+    #printing to confirm
+    print(gridX)
+    print(gridY)
+    print(posX)
+    print(posY)
 
-    quality = network['Quality']
-    quality = int(quality)  #get numeric value
+    #upload netowrks
+    for network in networks:
+        print(network['Name'])    
+        name = network['Name']
+        address = network['Address']
 
-    channel = network['Channel']
-    channel = int(channel)  #get numeric value
+        signalLevel = network['Signal Level']
+        signalLevel = int(signalLevel) #get numeric value
 
-    frequency = network['Frequency']
-    frequency = float(frequency) #get numeric value
+        bitRates = network['Bit Rates']
 
-    encryption = network['Encryption']
+        #changes bit rate string to array of values
+        bitRates = getBitRatesVals(bitRates) 
 
-    
-    r = requests.post(endpoint, json={'name' : name, 'address': address, 'signalLevel': signalLevel, 'bitRates' : bitRates, 'quality' : quality, 'channel' : channel, 'frequency' : frequency, 'encryption' : encryption})
-    print(r.status_code, r.reason)
+        quality = network['Quality']
+        quality = int(quality)  #get numeric value
+
+        channel = network['Channel']
+        channel = int(channel)  #get numeric value
+
+        frequency = network['Frequency']
+        frequency = float(frequency) #get numeric value
+
+        encryption = network['Encryption']
+
+       
+
+        r = requests.post(endpoint, json={'name' : name, 'address': address, 'signalLevel': signalLevel, 'bitRates' : bitRates, 'quality' : quality, 'channel' : channel, 'frequency' : frequency, 'encryption' : encryption})
+        r = requests.post(POSendpoint, json={'name' : name, 'address': address, 'signalLevel': signalLevel, 'bitRates' : bitRates, 'quality' : quality, 'channel' : channel, 'frequency' : frequency, 'encryption' : encryption, 'gridX' : gridX, 'gridY' : gridY, 'posX' : posX, 'posY' : posY})
+        print(r.status_code, r.reason)
+finally:
+    f.close()
+time.sleep(10)
