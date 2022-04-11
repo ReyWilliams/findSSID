0175b8961e6f37d122e8625ffc14b0940671f82e
Rey Williams <reynaldomwilliams@gmail.com>
2022-04-11 15:36:03 -0400

    update init shell script to do all commands at once

--- scripts/init_scripts.sh
+++ scripts/init_scripts.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd /home/pi/Desktop/Parsing
-python fetch_command.py
-python acknowledge.py
-python parsing.py
+cd /home/pi/Desktop/Parsing &
+python fetch_command.py &
+python acknowledge.py &
+python parsing.py &
 echo "Ran Scripts"
-
